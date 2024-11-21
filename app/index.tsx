fd87c93957d548413953384b173b47f73ba59680
Kasper Isager Dalsgarð <kasperisager@hey.com>
2024-11-21 11:42:50 +0100

    Switch to Prettier and update example
    Closes #6

--- app/index.tsx
+++ app/index.tsx
@@ -2,32 +2,41 @@ import { useState, useEffect } from 'react'
 import { Text } from 'react-native'
 import { Worklet } from 'react-native-bare-kit'
 
-export default function() {
+export default function () {
   const [response, setReponse] = useState<string | null>(null)
 
   useEffect(() => {
     const worklet = new Worklet()
 
-    worklet.start('/app.js', `
+    worklet.start(
+      '/app.js',
+      `
       const rpc = new BareKit.RPC((req) => {
         if (req.command === 'ping') {
-          console.log(req.data.toString())
-
-          req.reply('Hello from Bare!')
+          req.reply('Pong from Bare!')
         }
       })
-    `)
 
-    const rpc = new worklet.RPC(() => { /* No reply */ })
+      const req = rpc.request('ping')
+
+      req.send('Ping from Bare!')
+
+      req.reply('utf8').then((res) => console.log(res))
+      `
+    )
+
+    const rpc = new worklet.RPC((req) => {
+      if (req.command === 'ping') {
+        req.reply('Pong from React Native!')
+      }
+    })
 
     const req = rpc.request('ping')
 
-    req.send('Hello from React Native!')
+    req.send('Ping from React Native!')
 
     req.reply('utf8').then((res: string) => setReponse(res))
   }, [])
 
-  return (
-    <Text>{response}</Text>
-  )
+  return <Text>{response}</Text>
 }
